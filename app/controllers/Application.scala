c20e7cb49f6a1d92de951f2f140e41c406e5e6a2
Tom <twang10@gmail.com>
2014-10-18 17:54:46 -0400

    route and model test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,22 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import views._
+import models._
+
 object Application extends Controller {
 
   def index = Action {
+    
+    val sexy = new Multiverse("goog")
+    sexy.sendGet()
     Ok(views.html.index("Your new application is ready."))
+
   }
+
+  def sexy(id: String) = Action {
+  	Ok(views.html.index(id))
+  } 
+
 
 }
