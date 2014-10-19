a328af2960906d37ace683f95fcf157b3f2db510
nusoff01 <nicholas.usoff@tufts.edu>
2014-10-18 21:11:57 -0400

    added test function for bloomberg

--- app/models/Multiverse.java
+++ app/models/Multiverse.java
@@ -61,6 +61,63 @@ public class Multiverse{
 		//print result
 		System.out.println(response.toString());
 	}
+	
+
+	private void run() throws Exception
+    {
+        
+        int serverPort = 8194;
+
+        SessionOptions sessionOptions = new SessionOptions();
+        String serverHost = "10.8.8.1";
+        sessionOptions.setServerHost(serverHost);
+        sessionOptions.setServerPort(serverPort);
+
+        System.out.println("Connecting to " + serverHost + ":" + serverPort);
+        Session session = new Session(sessionOptions);
+        if (!session.start()) {
+            System.err.println("Failed to start session.");
+            return;
+        }
+        if (!session.openService("//blp/refdata")) {
+            System.err.println("Failed to open //blp/refdata");
+            return;
+        }
+        Service refDataService = session.getService("//blp/refdata");
+        Request request = refDataService.createRequest("HistoricalDataRequest");
+
+        Element securities = request.getElement("securities");
+        securities.appendValue("IBM US Equity");
+        securities.appendValue("MSFT US Equity");
+
+        Element fields = request.getElement("fields");
+        fields.appendValue("PX_LAST");
+        fields.appendValue("OPEN");
+
+        request.set("periodicityAdjustment", "ACTUAL");
+        request.set("periodicitySelection", "MONTHLY");
+        request.set("startDate", "20060101");
+        request.set("endDate", "20061231");
+        request.set("maxDataPoints", 100);
+        request.set("returnEids", true);
+
+        System.out.println("Sending Request: " + request);
+        session.sendRequest(request, null);
+
+        while (true) {
+            Event event = session.nextEvent();
+            MessageIterator msgIter = event.messageIterator();
+            while (msgIter.hasNext()) {
+                Message msg = msgIter.next();
+                System.out.println(msg);
+            }
+            if (event.eventType() == Event.EventType.RESPONSE) {
+                break;
+            }
+        }
+    }
+	
+	
     
     
 }
